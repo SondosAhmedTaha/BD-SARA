21ef6f8e81e1a6ef10f445e419f111d413cd1064
Sondos Taha <129762487+SondosAhmedTaha@users.noreply.github.com>
2025-07-22 18:30:50 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ artFile = "example"
 speed = 0.005
 # Print code in the beginning
 codePrint = False
-codingSpeed = 0.01
+codingSpeed = 0.000005
 codeColor='red'
 # Audio
 playAudio = True
 audio = 'HappyBirthday.mp3'
 # Random color is choosen from the list
-color = ['red','green','yellow','blue','magenta','cyan','white']
+color = ['red','pink','brown','blue','magenta','cyan','white']
 # Change the keys of the dict to change the color codes
 # If you change the color codes for blink, remove blink(none) and random, you have to change it in pprint() function of PyBirthdayWish.py too.
-colorCodes = {'①':'grey','②':'red','③':'green','④':'yellow','⑤':'blue','⑥':'magenta','⑦':'cyan','⑧':'white','⑨':'blink','⑩':'none','®':'random'}
+colorCodes = {'①':'pink','②':'red','③':'pink','④':'brown','⑤':'blue','⑥':'magenta','⑦':'cyan','⑧':'white','⑨':'blink','⑩':'none','®':'random'}
